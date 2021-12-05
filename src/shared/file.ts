52a10fba679b2bf405cd5b73d01dc0b586ffa89d
Rob Williams <rwilliams@spotify.com>
2021-12-04 21:51:33 -0500

    some basic setup and day 1

---
+++ src/shared/file.ts
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import readline from 'readline';
+
+export async function openFileReader(fileName: string): Promise<readline.Interface> {
+  const fileStream = fs.createReadStream(fileName);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  return rl;
+}
